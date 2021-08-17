251cc5e03a87a4621ec83a020334a0f43348bfdf
halezmarie <teacherhaleymarie@gmail.com>
2021-08-17 17:55:49 -0500

    commit tree 03749dff62dc68d6c550e2db9c4b5bd63fc78c58

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const driver = {};
+
+// updateDriverWithKeyAndValue()- this function should take in three arguments: a driver Object, a key and a value. This function should not mutate the driver and should return a new driver that has an updated value for the key passed in.
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver};
+    newDriver[key] = value;
+    return newDriver;
+}
+
+// destructivelyUpdateDriverWithKeyAndValue() - this function should work the same as updateDriverWithKeyAndValue() but it should mutate the driver parameter passed in.
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
+}
+
+// deleteFromDriverByKey() - this function should take in a driver Object and a key. It should delete the key/value pair for the key that was passed in from the driver Object. This should all not actually mutate the driver passed in.
+
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = {...driver};
+    delete newDriver[key];
+    return newDriver;
+}
+
+// destructivelyDeleteFromDriverByKey() - this function should work the same as deleteFromDriverByKey() but it should mutate the driver passed in. 
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
